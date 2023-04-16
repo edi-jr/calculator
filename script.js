d472884458007f070c113e73324e0c848d6e23e4
Edi <128224911+edijrdev@users.noreply.github.com>
2023-04-16 09:38:40 -0300

    Make the calculator work

--- script.js
+++ script.js
@@ -15,6 +15,9 @@ function divide(x,y) {
 }
 
 function operate(n1, n2, op) {
+  n1 = Number(lastScreen.textContent);
+  n2 = Number(currentScreen.textContent);
+  op = signScreen.textContent;
   switch(op) {
     case "+":
       return add(n1, n2);
@@ -23,8 +26,55 @@ function operate(n1, n2, op) {
     case "*":
       return multiply(n1, n2);
     case "/":
-      return divide(n1, n2);
+      if(n2 !== 0) return divide(n1, n2);
   }
 }
 
-let num1, num2, operator;
+function handleDigit(value) {
+  currentScreen.textContent += value;
+  if(lastScreen.textContent !== "" && currentScreen.textContent) {
+    resultScreen.textContent = operate();
+  }
+}
+
+function handleOperator(value) {
+  if(currentScreen.textContent === "" && lastScreen.textContent === "") return;
+  if(signScreen.textContent === "") {
+    lastScreen.textContent = currentScreen.textContent;
+  } else if(currentScreen.textContent !== "" && resultScreen.textContent !== "") {
+    lastScreen.textContent = operate();
+    resultScreen.textContent = "";
+  }
+  signScreen.textContent = value;
+  currentScreen.textContent = "";
+}
+
+function handleEqualButton() {
+  if(resultScreen.textContent !== "") {
+    currentScreen.textContent = resultScreen.textContent;
+    lastScreen.textContent = "";
+    signScreen.textContent = "";
+    resultScreen.textContent = "";
+  }
+}
+
+function clearAll() {
+  lastScreen.textContent = "";
+  currentScreen.textContent = "";
+  signScreen.textContent = "";
+  resultScreen.textContent = "";
+}
+
+const lastScreen = document.querySelector("#last");
+const currentScreen = document.querySelector("#current");
+const signScreen = document.querySelector("#sign");
+const resultScreen = document.querySelector("#result");
+const digitButtons = document.querySelectorAll(".digit");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalButton = document.querySelector("#equal");
+const allClearButton = document.querySelector("#all-clear");
+
+digitButtons.forEach(button => button.addEventListener("click", e => handleDigit(e.target.value)));
+operatorButtons.forEach(operator => operator.addEventListener("click", e => handleOperator(e.target.value)));
+equalButton.addEventListener("click", handleEqualButton);
+allClearButton.addEventListener("click", clearAll);
